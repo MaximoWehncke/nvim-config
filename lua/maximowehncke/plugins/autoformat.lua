df9b4ee92afaabed08ecf241a111c606c3fd29b5
MaximoWehncke <mwehncke@itba.edu.ar>
2025-12-25 17:53:01 -0300

    disable java formatting

--- lua/maximowehncke/plugins/autoformat.lua
+++ lua/maximowehncke/plugins/autoformat.lua
@@ -10,7 +10,7 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don€ý,t
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { c = true, cpp = true, java = true }
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
 				else
