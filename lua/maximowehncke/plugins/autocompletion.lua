fac52fdebca3d11726d7f215f946578eb79a2191
MaximoWehncke <mwehncke@itba.edu.ar>
2025-12-25 17:51:04 -0300

    use supermaven which doesn't expire like copilot

--- lua/maximowehncke/plugins/autocompletion.lua
+++ lua/maximowehncke/plugins/autocompletion.lua
@@ -57,60 +57,62 @@ return {
 			signature = { enabled = true },
 		},
 	},
-	{
-		"github/copilot.vim",
-		priority = 999,
-		event = "InsertEnter",
-		init = function()
-			vim.g.copilot_no_tab_map = true
-			vim.g.copilot_assume_mapped = true
-		end,
-		config = function()
-			local keymap = vim.keymap
-			vim.g.copilot_no_tab_map = true
-
-			vim.keymap.set("i", "<S-Tab>", function()
-				return vim.fn["copilot#Accept"]("")
-			end, {
-				expr = true,
-				replace_keycodes = false,
-				desc = "Accept Copilot suggestion",
-			})
-			keymap.set(
-				"n",
-				"<leader>cs",
-				"<cmd>Copilot disable<CR><cmd>echo 'Copilot suggestion disabled'<CR>",
-				{ desc = "Disable Copilot suggestions", silent = true }
-			)
-			keymap.set(
-				"n",
-				"<leader>ce",
-				"<cmd>Copilot enable<CR><cmd>echo 'Copilot suggestion enabled'<CR>",
-				{ desc = "Enable Copilot suggestions", silent = true }
-			)
-		end,
-	},
 	-- {
-	-- 	"supermaven-inc/supermaven-nvim",
+	-- 	"github/copilot.vim",
+	-- 	priority = 999,
+	-- 	event = "InsertEnter",
+	-- 	init = function()
+	-- 		vim.g.copilot_no_tab_map = true
+	-- 		vim.g.copilot_assume_mapped = true
+	-- 	end,
 	-- 	config = function()
-	-- 		require("supermaven-nvim").setup({
-	-- 			keymaps = {
-	-- 				accept_suggestion = "<S-Tab>",
-	-- 				clear_suggestion = "<C-]>",
-	-- 				accept_word = "<C-j>",
-	-- 			},
-	-- 			ignore_filetypes = { cpp = true }, -- or { "cpp", }
-	-- 			color = {
-	-- 				suggestion_color = "#767676",
-	-- 				cterm = 244,
-	-- 			},
-	-- 			log_level = "info", -- set to "off" to disable logging completely
-	-- 			disable_inline_completion = false, -- disables inline completion for use with cmp
-	-- 			disable_keymaps = false, -- disables built in keymaps for more manual control
-	-- 			condition = function()
-	-- 				return false
-	-- 			end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+	-- 		local keymap = vim.keymap
+	-- 		vim.g.copilot_no_tab_map = true
+	--
+	-- 		vim.keymap.set("i", "<S-Tab>", function()
+	-- 			return vim.fn["copilot#Accept"]("")
+	-- 		end, {
+	-- 			expr = true,
+	-- 			replace_keycodes = false,
+	-- 			desc = "Accept Copilot suggestion",
 	-- 		})
+	-- 		keymap.set(
+	-- 			"n",
+	-- 			"<leader>cs",
+	-- 			"<cmd>Copilot disable<CR><cmd>echo 'Copilot suggestion disabled'<CR>",
+	-- 			{ desc = "Disable Copilot suggestions", silent = true }
+	-- 		)
+	-- 		keymap.set(
+	-- 			"n",
+	-- 			"<leader>ce",
+	-- 			"<cmd>Copilot enable<CR><cmd>echo 'Copilot suggestion enabled'<CR>",
+	-- 			{ desc = "Enable Copilot suggestions", silent = true }
+	-- 		)
 	-- 	end,
 	-- },
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<S-Tab>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
+				},
+				ignore_filetypes = { cpp = true }, -- or { "cpp", }
+				color = {
+					suggestion_color = "#767676",
+					cterm = 244,
+				},
+				log_level = "info", -- set to "off" to disable logging completely
+				disable_inline_completion = false, -- disables inline completion for use with cmp
+				disable_keymaps = false, -- disables built in keymaps for more manual control
+				condition = function()
+					return false
+				end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+
+        vim.keymap.set("n", "<leader>cst", "<cmd>SupermavenToggle<CR><cmd>echo 'SuperMaven toggled'<CR>", {desc = "Toggle supermacen suggestions on/off"})
+			})
+		end,
+	},
 }
