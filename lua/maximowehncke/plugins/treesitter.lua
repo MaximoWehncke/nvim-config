2e94810b657a09fb50ea938c1dede8fdf9aae6da
MaximoWehncke <mwehncke@itba.edu.ar>
2026-02-19 17:22:46 +0100

    feat(treesitter): tag renaming and closing

--- lua/maximowehncke/plugins/treesitter.lua
+++ lua/maximowehncke/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
 		dependencies = {
 			"windwp/nvim-ts-autotag",
 			"JoosepAlviste/nvim-ts-context-commentstring",
+			"andymass/vim-matchup",
 		},
 		config = function()
 			-- disable deprecated module
@@ -15,13 +16,26 @@ return {
 				enable_autocmd = false,
 			})
 
-			local treesitter = require("nvim-treesitter.configs")
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				per_filetype = {
+					["html"] = {
+						enable_close = false,
+					},
+				},
+			})
 
+			local treesitter = require("nvim-treesitter.configs")
 			treesitter.setup({
 				highlight = { enable = true },
 				indent = { enable = true },
-				autotag = { enable = true },
-
+				-- REMOVED: autotag = { enable = true }, -- This line is deprecated!
 				ensure_installed = {
 					"c",
 					"lua",
@@ -53,86 +67,83 @@ return {
 						node_decremental = "<bs>",
 					},
 				},
+				-- matchup integration
+				matchup = {
+					enable = true,
+				},
 			})
 		end,
 	},
 	{
-		{
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			dependencies = { "nvim-treesitter/nvim-treesitter" },
-			config = function()
-				require("nvim-treesitter.configs").setup({
-					textobjects = {
-						select = {
-							enable = true,
-							-- Automatically jump forward to textobj, similar to targets.vim
-							lookahead = true,
-							keymaps = {
-								-- You can use the capture groups defined in textobjects.scm
-								["af"] = "@function.outer",
-								["if"] = "@function.inner",
-								["ac"] = "@class.outer",
-								["ic"] = "@class.inner",
-								["aa"] = "@parameter.outer",
-								["ia"] = "@parameter.inner",
-								["ab"] = "@block.outer",
-								["ib"] = "@block.inner",
-								["al"] = "@loop.outer",
-								["il"] = "@loop.inner",
-								["ai"] = "@conditional.outer",
-								["ii"] = "@conditional.inner",
-								["a/"] = "@comment.outer",
-								["i/"] = "@comment.inner",
-							},
-						},
-
-						move = {
-							enable = true,
-							set_jumps = true, -- whether to set jumps in the jumplist
-							goto_next_start = {
-								["]f"] = "@function.outer",
-								["]c"] = "@class.outer",
-								["]a"] = "@parameter.inner",
-							},
-							goto_next_end = {
-								["]F"] = "@function.outer",
-								["]C"] = "@class.outer",
-								["]A"] = "@parameter.inner",
-							},
-							goto_previous_start = {
-								["[f"] = "@function.outer",
-								["[c"] = "@class.outer",
-								["[a"] = "@parameter.inner",
-							},
-							goto_previous_end = {
-								["[F"] = "@function.outer",
-								["[C"] = "@class.outer",
-								["[A"] = "@parameter.inner",
-							},
-						},
-
-						swap = {
-							enable = true,
-							swap_next = {
-								["<leader>a"] = "@parameter.inner",
-							},
-							swap_previous = {
-								["<leader>A"] = "@parameter.inner",
-							},
-						},
-
-						lsp_interop = {
-							enable = true,
-							border = "none",
-							floating_preview_opts = {},
-							peek_definition_code = {
-								["<leader>df"] = "@function.outer",
-								["<leader>dF"] = "@class.outer",
-							},
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["ab"] = "@block.outer",
+							["ib"] = "@block.inner",
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+							["ai"] = "@conditional.outer",
+							["ii"] = "@conditional.inner",
+							["a/"] = "@comment.outer",
+							["i/"] = "@comment.inner",
 						},
 					},
-				})
-			end,
-		},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]C"] = "@class.outer",
+							["]A"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+							["[a"] = "@parameter.inner",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[C"] = "@class.outer",
+							["[A"] = "@parameter.inner",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+					lsp_interop = {
+						enable = true,
+						border = "none",
+						floating_preview_opts = {},
+						peek_definition_code = {
+							["<leader>df"] = "@function.outer",
+							["<leader>dF"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
 	},
 }
