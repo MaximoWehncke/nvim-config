f00bc967a973086c6a489587fa26587b43654128
MaximoWehncke <mwehncke@itba.edu.ar>
2026-02-19 17:31:42 +0100

    feat(telescope): search git edited files

--- lua/maximowehncke/plugins/telescope.lua
+++ lua/maximowehncke/plugins/telescope.lua
@@ -3,24 +3,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
 	event = "VimEnter",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"axkirillov/telescope-changed-files", -- Add this dependency
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 			"nvim-telescope/telescope-fzf-native.nvim",
-			-- `build` is used to run some command when the plugin is installed/updated.
-			-- This is only run then, not every time Neovim starts up.
 			build = "make",
-			-- `cond` is a condition used to determine whether this plugin should be
-			-- installed and loaded.
 			cond = function()
 				return vim.fn.executable("make") == 1
 			end,
 		},
 		{ "nvim-telescope/telescope-ui-select.nvim" },
-		-- Useful for getting pretty icons, but requires a Nerd Font.
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
 		-- [[ Configure Telescope ]]
-		-- See `:help telescope` and `:help telescope.setup()`
 		require("telescope").setup({
 			defaults = {
 				mappings = {
@@ -44,15 +39,26 @@ return { -- Fuzzy Finder (files, lsp, etc)
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
 				},
+				-- Configure changed_files extension
+				changed_files = {
+					-- Show changes compared to what? Options: HEAD, index, unstaged
+					show_diff_base = "HEAD",
+				},
 			},
 		})
-		-- Enable Telescope extensions if they are installed
+
+		-- Enable Telescope extensions
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
-		-- See `:help telescope.builtin`
+		pcall(require("telescope").load_extension, "changed_files")
+
+		-- Keymaps
 		local builtin = require("telescope.builtin")
 		local keymap = vim.keymap
 
+		keymap.set("n", "<leader>fg", builtin.git_status, { desc = "[F]ind [G]it changed files" })
+
+		-- Standard telescope keymaps
 		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
@@ -62,23 +68,20 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>fb", function()
-			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 				winblend = 10,
 				previewer = false,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
-		-- It's also possible to pass additional configuration options.
-		--  See `:help telescope.builtin.live_grep()` for information about particular keys
+
 		vim.keymap.set("n", "<leader>s/", function()
 			builtin.live_grep({
 				grep_open_files = true,
 				prompt_title = "Live Grep in Open Files",
 			})
 		end, { desc = "[S]earch [/] in Open Files" })
-		-- Shortcut for searching your Neovim configuration files
+
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
 		end, { desc = "[S]earch [N]eovim files" })
