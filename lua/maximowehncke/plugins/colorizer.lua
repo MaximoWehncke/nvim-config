aa6bc5ec39fabfcd487d83c01b4a0ea4496e1d72
MaximoWehncke <mwehncke@itba.edu.ar>
2026-04-25 15:14:56 +0200

    change the color renderer

--- lua/maximowehncke/plugins/colorizer.lua
+++ lua/maximowehncke/plugins/colorizer.lua
@@ -1,9 +1,3 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup({
-			"*",
-			css = { rgb_fn = true },
-		})
-	end,
+	"brenoprata10/nvim-highlight-colors",
 }
