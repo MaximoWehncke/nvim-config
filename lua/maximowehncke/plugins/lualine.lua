108f2f57feb42cd5a8315e8d5430680c800d34f6
MaximoWehncke <mwehncke@itba.edu.ar>
2026-04-25 17:01:10 +0200

    final lualine + vim-tpipeline

--- lua/maximowehncke/plugins/lualine.lua
+++ lua/maximowehncke/plugins/lualine.lua
@@ -4,7 +4,7 @@ local function supermaven_status()
 		return ""
 	end
 	if api.is_running() then
-		return "󰓞"
+		return "󰠖"
 	else
 		return ""
 	end
@@ -35,92 +35,104 @@ local function fileicon()
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"chrisgrieser/nvim-recorder",
+	{
+		"vimpostor/vim-tpipeline",
+		init = function()
+			vim.g.tpipeline_split = 0
+			vim.g.tpipeline_clearstl = 1
+			vim.g.tpipeline_autoempty = 1 -- auto empty when pane is not focused
+		end,
 	},
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status")
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"chrisgrieser/nvim-recorder",
+		},
+		config = function()
+			local lualine = require("lualine")
+			local lazy_status = require("lazy.status")
 
-		-- configure lualine with modified theme
-		lualine.setup({
-			options = {
-				theme = "auto",
-				disabled_filetypes = { "packer", "NvimTree" },
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-				globalstatus = false,
-				padding = 4,
-				always_divide_middle = false,
-			},
-			winbar = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			inactive_winbar = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = {},
-				lualine_c = {
-					"branch",
-					fileicon(),
-					{
-						function()
-							return " "
-						end,
-						padding = { left = 0, right = 0 },
+			-- configure lualine with modified theme
+			lualine.setup({
+				options = {
+					theme = "auto",
+					disabled_filetypes = { "packer", "NvimTree" },
+					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+					globalstatus = true,
+					padding = 4,
+					always_divide_middle = false,
+				},
+				winbar = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_winbar = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {},
+					lualine_c = {
+						"branch",
+						fileicon(),
+						{
+							function()
+								return " "
+							end,
+							padding = { left = 0, right = 0 },
+						},
+						{
+							"filename",
+							padding = { left = 0, right = 1 },
+						},
+						{
+							lazy_status.updates,
+							cond = lazy_status.has_updates,
+							color = { fg = "#ff9e64" },
+							padding = { left = 2, right = 1 },
+						},
+						{
+							supermaven_status,
+							color = function()
+								local ok, api = pcall(require, "supermaven-nvim.api")
+								if ok and api.is_running() then
+									return { fg = "#FF7A65" }
+								else
+									return { fg = "#ea6962" }
+								end
+							end,
+							padding = { left = 0, right = 1 },
+						},
 					},
-					{
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {
+						fileicon(),
 						"filename",
-						padding = { left = 0, right = 1 },
 					},
-					{
-						supermaven_status,
-						color = function()
-							local ok, api = pcall(require, "supermaven-nvim.api")
-							if ok and api.is_running() then
-								return { fg = "#28FF2C" }
-							else
-								return { fg = "#ea6962" }
-							end
-						end,
-					},
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
 				},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {
-					fileicon(),
-					"filename",
-				},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			extensions = {},
-		})
-		vim.opt.laststatus = 0
-	end,
+				extensions = {},
+			})
+			-- vim.opt.laststatus = 0
+		end,
+	},
 }
