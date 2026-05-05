c1799ff0da59696e37b253a306ede0cb56e98803
MaximoWehncke <mwehncke@itba.edu.ar>
2026-05-05 11:50:43 +0200

    feat: added some themes and delete custom tokyonight colors

--- lua/maximowehncke/plugins/themify.lua
+++ lua/maximowehncke/plugins/themify.lua
@@ -17,35 +17,6 @@ return {
 					"folke/tokyonight.nvim",
 					priority = 1000,
 					config = function()
-						local bg = "#011628"
-						local bg_dark = "#011423"
-						local bg_highlight = "#143652"
-						local bg_search = "#0A64AC"
-						local bg_visual = "#275378"
-						local fg = "#CBE0F0"
-						local fg_dark = "#B4D0E9"
-						local fg_gutter = "#627E97"
-						local border = "#547998"
-						require("tokyonight").setup({
-							style = "night",
-							on_colors = function(colors)
-								colors.bg = bg
-								colors.bg_dark = bg_dark
-								colors.bg_float = bg_dark
-								colors.bg_highlight = bg_highlight
-								colors.bg_popup = bg_dark
-								colors.bg_search = bg_search
-								colors.bg_sidebar = bg_dark
-								colors.bg_statusline = bg_dark
-								colors.bg_visual = bg_visual
-								colors.border = border
-								colors.fg = fg
-								colors.fg_dark = fg_dark
-								colors.fg_float = fg
-								colors.fg_gutter = fg_gutter
-								colors.fg_sidebar = fg_dark
-							end,
-						})
 						vim.cmd([[colorscheme tokyonight]])
 					end,
 				},
@@ -69,7 +40,47 @@ return {
 						vim.cmd("colorscheme rose-pine")
 					end,
 				},
-				{ "sainnhe/sonokai" },
+				{
+					"zitrocode/carvion.nvim",
+					priority = 1000,
+					config = function()
+						vim.cmd("colorscheme carvion")
+					end,
+				},
+				{
+					"tiagovla/tokyodark.nvim",
+					name = "tokyodark",
+					priority = 1000,
+					config = function()
+						vim.cmd("colorscheme tokyodark")
+					end,
+				},
+				{
+					"AlexvZyl/nordic.nvim",
+					name = "nordic",
+					priority = 1000,
+					config = function()
+						vim.cmd("colorscheme nordic")
+					end,
+				},
+				{
+					"sainnhe/sonokai",
+					name = "sonokai",
+					priority = 1000,
+					config = function()
+						vim.cmd("colorscheme sonokai")
+					end,
+				},
+				{
+					"navarasu/onedark.nvim",
+					priority = 1000, -- make sure to load this before all the other start plugins
+					config = function()
+						require("onedark").setup({
+							style = "darker",
+						})
+						require("onedark").load()
+					end,
+				},
 				"default",
 			})
 		end,
